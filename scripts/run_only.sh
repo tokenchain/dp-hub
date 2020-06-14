67c826bbe82c27dad71a1195bd0d36a4e368ab22
jhes <ooxfordck@gmail.com>
2020-06-14 23:54:49 +0800

    remove some files

--- scripts/run_only.sh
+++ scripts/run_only.sh
@@ -9,3 +9,4 @@ ixocli rest-server --chain-id darkpool-1x --trust-node && fg
 #suit issue burden aware heart romance key orphan project idea hammer cloth
 #chuenkam
 #========
+#p2p.persistent_peers=192.168.1.3:46656,192.168.1.4:46656,192.168.1.5:46656\ --home
