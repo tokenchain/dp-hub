9945880c0359a52f7687fc7ed83c8b19e5fc0c36
jjhesk <ooxfordck@gmail.com>
2020-07-15 18:51:42 +0800

    🍯 Compile success edition for v1.3.218

--- x/did/internal/types/codec.go
+++ x/did/internal/types/codec.go
@@ -7,17 +7,20 @@ import (
 )
 
 func RegisterCodec(cdc *codec.Codec) {
-	cdc.RegisterConcrete(MsgAddDid{}, "did/AddDid", nil)
-	cdc.RegisterConcrete(MsgAddCredential{}, "did/AddCredential", nil)
+	cdc.RegisterConcrete(MsgAddDid{}, "darkpool/MsgAddDid", nil)
+	cdc.RegisterConcrete(MsgAddCredential{}, "darkpool/MsgAddCredential", nil)
+	// TODO: https://github.com/tokenchain/ixo-blockchain/issues/76
+	cdc.RegisterConcrete(BaseDidDoc{}, "darkpool/BaseDidDoc", nil)
 	cdc.RegisterInterface((*exported.DidDoc)(nil), nil)
 	cdc.RegisterInterface((*exported.IdpDid)(nil), nil)
-	// TODO: https://github.com/tokenchain/ixo-blockchain/issues/76
-	cdc.RegisterConcrete(BaseDidDoc{}, "did/BaseDidDoc", nil)
+	cdc.RegisterInterface((*ante.IxoTx)(nil), nil)
+	cdc.RegisterInterface((*ante.IxoSignature)(nil), nil)
+
 	//cdc.RegisterConcrete(DidCredential{}, "did/DidCredential", nil)
-	cdc.RegisterConcrete(exported.Claim{}, "did/Claim", nil)
-	cdc.RegisterConcrete(ante.IxoSignature{}, "darkpool-dxp/Signature", nil)
-	cdc.RegisterConcrete(ante.IxoTx{}, "darkpool-dxp/Transactions", nil)
-	cdc.RegisterInterface((*ante.TxActor)(nil), nil)
+	//	cdc.RegisterConcrete(exported.Claim{}, "did/Claim", nil)
+	//	cdc.RegisterConcrete(ante.IxoSignature{}, "darkpool-dxp/Signature", nil)
+	//	cdc.RegisterConcrete(ante.IxoTx{}, "darkpool-dxp/Transactions", nil)
+	//	cdc.RegisterInterface((*ante.TxActor)(nil), nil)
 
 }
 
